c350952ee954adb7703580a284069b43f471239f
Abhi-Bhat18 <kishore1514@gmail.com>
2022-12-10 05:17:48 +0530

    nothing

--- src/components/Recommendations.tsx
+++ src/components/Recommendations.tsx
@@ -4,6 +4,13 @@ import Table from "./Table";
 const Recommendations = () => {
 
   const [Data, setData] = useState([])
+  const category = ['Data Science', 'HR', 'Advocate', 'Arts', 'Web Designing',
+       'Mechanical Engineer', 'Sales', 'Health and fitness',
+       'Civil Engineer', 'Java Developer', 'Business Analyst',
+       'SAP Developer', 'Automation Testing', 'Electrical Engineering',
+       'Operations Manager', 'Python Developer', 'DevOps Engineer',
+       'Network Security Engineer', 'PMO', 'Database', 'Hadoop',
+       'ETL Developer', 'DotNet Developer', 'Blockchain', 'Testing']
 
   const getRecommendations = async () => {
     fetch('http://localhost:5000/recommendation', {
@@ -45,22 +52,27 @@ const Recommendations = () => {
             className="flex flex-row flex-wrap"
             onSubmit={getRecommendations}
           >
-            <div className="flex justify-center">
+            <div className="flex justify-center items-center">
               <div className="mb-3 xl:w-96">
                 <select
                   className="form-select appearance-none block w-full px-3 py-1.5 text-basefont-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0
       focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" aria-label="Default select example"
                 >
-                  <option selected>Open this select menu</option>
-                  <option value="1">One</option>
+                  {/* <option value="1">One</option>
                   <option value="2">Two</option>
-                  <option value="3">Three</option>
+                <option value="3">Three</option> */}
+                <option selected>Select a Category</option>
+                  {
+                    category.map((ele,index)=>{
+                      <option value={index}>{ele}</option>
+                    })
+                  }
                 </select>
               </div>
             </div>
             <button
               type="submit"
-              className="w-2/5 p-3 font-semibold rounded-r-lg sm:w-1/3 dark:bg-violet-400 dark:text-gray-900"
+              className="w-2/5 p-3 font-semibold rounded-r-lg sm:w-1/3 dark:bg-violet-400 dark:text-gray-900 flex justify-center items-center"
             >
               Get applicants
             </button>
