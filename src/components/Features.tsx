58b2f9cdb30c8a2bfc27e4db2f85521d82940214
Abhi-Bhat18 <kishore1514@gmail.com>
2022-12-10 07:32:00 +0530

    feature

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -19,11 +19,12 @@ function Features() {
 
         <div className="mt-8 grid grid-cols-1 gap-8 md:mt-16 md:grid-cols-2 md:gap-12 lg:grid-cols-3">
           <Feature/>
-          <Feature/>
-          <Feature/>
-          <Feature/>
-          <Feature/>
-          <Feature/>
+          <Feature feature="NLP and Cosine similarity"/>
+          <Feature feature="Real time hiring"/>
+          <Feature feature="Easy to Use"/>
+          <Feature feature="Easy to apply"/>
+          <Feature feature="More reliable"/>
+
         </div>
       </div>
     </section>
